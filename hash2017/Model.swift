e9f36728ea7f7e34fc45b00e0ba2d1f5737cfa2e
Pascal Batty <pascal.batty@soat.fr>
2017-02-23 20:05:33 +0100

    Now using interconnected classes for model

--- hash2017/Model.swift
+++ hash2017/Model.swift
@@ -10,40 +10,88 @@ import Foundation
 
 typealias Megabytes = Double
 
-struct Video {
+class Video: Hashable, Equatable {
     let identifier: Int
     let size: Megabytes
+
+    init(identifier: Int, size: Megabytes) {
+        self.identifier = identifier
+        self.size = size
+    }
+
+    var hashValue: Int { return identifier }
+
+    static func == (lhs: Video, rhs: Video) -> Bool {
+        return lhs.identifier == rhs.identifier
+    }
 }
 
-struct CacheServer {
+class CacheServer: Hashable, Equatable {
     let identifier: Int
     let capacity: Megabytes
+
+    var videos = Set<Video>()
+
+    init(identifier: Int, capacity: Megabytes) {
+        self.identifier = identifier
+        self.capacity = capacity
+    }
+
+    var hashValue: Int { return identifier }
+
+    static func == (lhs: CacheServer, rhs: CacheServer) -> Bool {
+        return lhs.identifier == rhs.identifier
+    }
 }
 
-struct Endpoint {
+class Endpoint {
     let identifier: Int
     let latency: TimeInterval
-    let connections: [CacheConnection]
+    let connections: Set<CacheConnection>
+
+    init(identifier: Int, latency: TimeInterval, connections: [CacheConnection]) {
+        self.identifier = identifier
+        self.latency = latency
+        self.connections = Set(connections)
+    }
 }
 
-struct CacheConnection {
-    let serverId: Int
+class CacheConnection: Hashable, Equatable {
+    let server: CacheServer
     let latency: TimeInterval
+
+    init(server: CacheServer, latency: TimeInterval) {
+        self.server = server
+        self.latency = latency
+    }
+
+    var hashValue: Int { return server.hashValue }
+
+    static func == (lhs: CacheConnection, rhs: CacheConnection) -> Bool {
+        return lhs.server.identifier == rhs.server.identifier
+    }
 }
 
-struct RequestDescription {
-
-    let videoId: Int
-    let endpointId: Int
+class RequestDescription {
+    let video: Video
+    let endpoint: Endpoint
     let numberOfRequests: Int
+
+    init(video: Video, endpoint: Endpoint, numberOfRequests: Int) {
+        self.video = video
+        self.endpoint = endpoint
+        self.numberOfRequests = numberOfRequests
+    }
 }
 
 struct Model {
 
-    let videos: [Video]
-    let endpoints: [Endpoint]
+    let videos: [Int: Video]
+    let endpoints: [Int: Endpoint]
+    let cacheServers: [Int: CacheServer]
     let requestDescriptions: [RequestDescription]
-    let cacheServers: [CacheServer]
+
+    let cacheServerCapacity: Megabytes
 
     init(lines: [String]) {
 
@@ -60,15 +108,29 @@ struct Model {
         let numberOfRequestDescriptions = Int(counts[2])!
 
         let numberOfCacheServers = Int(counts[3])!
-        let capacityOfCacheServers = Megabytes(counts[4])!
 
-        cacheServers = (0..<numberOfCacheServers).map { CacheServer(identifier: $0, capacity: capacityOfCacheServers) }
+        cacheServerCapacity = Megabytes(counts[4])!
+
+        var cacheServers = [Int: CacheServer]()
+
+        for i in (0..<numberOfCacheServers) {
+            cacheServers[i] = CacheServer(identifier: i, capacity: cacheServerCapacity)
+        }
+
+        self.cacheServers = cacheServers
 
         let videosDescription = readNextLine()
 
-        videos = videosDescription.enumerated().map { Video(identifier: Int($0), size: Megabytes($1)!) }
+        var videos = [Int: Video]()
 
-        var endpoints = [Endpoint]()
+        for (i, videoDesc) in videosDescription.enumerated() {
+
+            videos[i] = Video(identifier: Int(i), size: Megabytes(videoDesc)!)
+        }
+
+        self.videos = videos
+
+        var endpoints = [Int: Endpoint]()
 
         for i in 0..<numberOfEndpoints {
             let endpointConfig = readNextLine()
@@ -81,10 +143,10 @@ struct Model {
                 let configConnection = readNextLine()
                 let serverId = Int(configConnection[0])!
                 let latency = TimeInterval(configConnection[1])!
-                connections.append(CacheConnection(serverId: serverId, latency: latency))
+                connections.append(CacheConnection(server: cacheServers[serverId]!, latency: latency))
             }
 
-            endpoints.append(Endpoint(identifier: i, latency: latency, connections: connections))
+            endpoints[i] = Endpoint(identifier: i, latency: latency, connections: connections)
         }
 
         self.endpoints = endpoints
@@ -99,8 +161,8 @@ struct Model {
             let endpointId = Int(requestDescConfig[1])!
             let numberOfRequests = Int(requestDescConfig[2])!
 
-            requestDescriptions.append(RequestDescription(videoId: videoId,
-                                                          endpointId: endpointId,
+            requestDescriptions.append(RequestDescription(video: videos[videoId]!,
+                                                          endpoint: endpoints[endpointId]!,
                                                           numberOfRequests: numberOfRequests))
         }
 
